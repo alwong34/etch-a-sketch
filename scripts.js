9ba672336957b666f0bce06c01beb8d574112275
Alex Wong <alexwong6295@gmail.com>
2022-08-30 18:14:11 -0700

    add functionality to change squares to black on click and mouse over

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
 // get the starting container we will put our grid into
 const container = document.getElementById("container");
+const gridWidth = 960;
+const numSquares = 16;
 
-for (let i = 0; i < 16; i++) {
+function changeColor(e) {
+    if (mouseDown > 0) {
+        e.style.backgroundColor = "black";
+    }
+}
+
+let mouseDown = 0;
+document.body.onmousedown = () => mouseDown++;
+document.body.onmouseup = () => mouseDown--;
+
+for (let i = 0; i < numSquares; i++) {
     //create 16 rows
     let gridRow = document.createElement('div');
     gridRow.classList.add("row");
@@ -11,21 +23,27 @@ for (let i = 0; i < 16; i++) {
         gridRow.style.borderTop = "solid black 1px";
     }
     // if the row is the last row, add an extra border on the bottom to match the other squares
-    else if (i == 15) {
+    else if (i == numSquares - 1) {
         gridRow.style.borderBottom = "solid black 1px";
     }
 
     // create 16 squares for each row
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < numSquares; j++) {
         let square = document.createElement('div');
         square.classList.add("square");
+        square.style.width = `${gridWidth / numSquares}px`;
+        square.style.height = `${gridWidth / numSquares}px`;
+
+        //change color on click and drag
+        square.onmousedown = () => square.style.backgroundColor = "black";
+        square.onmouseover = () => changeColor(square);
 
         // if it is the first square in the row, add a thicker border to match the other squares
         if (j == 0) {
             square.style.borderLeft = "solid black 2px";
         }
         // if it is the last square in the row, add a thicker border to match the other squares
-        else if (j == 15) {
+        else if (j == numSquares - 1) {
             square.style.borderRight = "solid black 2px";
         }
 
